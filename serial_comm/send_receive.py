9b1c1314e1e8dd9c1a24f60a3ae1af4fa55093bf
vortex2jm <joaopaulomoura5328@gmail.com>
2025-03-20 20:21:20 -0300

    :recycle: Refactoring seria comm

--- serial_comm/send_receive.py
+++ serial_comm/send_receive.py
@@ -3,10 +3,11 @@ import time
 import serial
 import threading
 
-PORT = "COM9"
+PORT = "COM4"
 BAUDRATE = 115200
 FEATURES_PATH = "./features_test.csv"
 
+# Loads the features vectors and its targets from csv file
 def load_features(path):
   features = []
   targets = []
@@ -20,6 +21,7 @@ def load_features(path):
   return (features, targets)
 
 
+# Routine that reads the corresponding serial port
 def serial_read(ser):
   while True:
     try:
@@ -30,27 +32,24 @@ def serial_read(ser):
       print(f"Read error: {e}")
       break
 
-# Função para enviar dados pela porta serial
+
+# Routine to send data by the corresponding serial port
 def serial_write(ser):
-  
+  # Loading features vectors
   features, _ = load_features(FEATURES_PATH)
   feature_index=0
 
   while True:
-    # inp = input()	# Type something into terminal to send features
-    # features = f"{1.39},{2.40},{3.41},{-1.20},{0.45},{2.71},{1.95},{-3.45},{1.00},{0.67},{1.34},{1.19},{-2.7}"
-    # if inp:
-    features_vector_string = "" # Creating empty string
-    
-    for value in features(feature_index):
-      features_vector_string+=f"{value},"
-    
-    features_vector_string = features_vector_string[:-1]
+    # Parsing vector to string
+    features_vector_string = ""
+    for value in features[feature_index]:
+      features_vector_string += f"{value:.2f},"  
+    features_vector_string = features_vector_string[:-1]    # Removing the last comma
 
     try:
-      time.sleep(0.5)
-      # ser.write(features_vector_string.encode("utf-8"))
-      print(features_vector_string)
+      time.sleep(0.5)   # Sliding window gap interval
+      print(f"\n\nFEATURES SENT: {features_vector_string}\n")
+      ser.write(features_vector_string.encode("utf-8"))
       feature_index+=1
 
     except serial.SerialException as e:
@@ -77,7 +76,6 @@ def main():
       
   except serial.SerialException as e:
     print(f"Serial port access error: {e}")
-
 
 if __name__ == "__main__":
     main()
